3d90e3286d8058861bef4316b939bdc0bd7266e8
jhonalex06 <jhonsigpjc@gmail.com>
2022-07-14 11:56:33 -0700

    Add dinamicMapLayers to the viewer

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ var map = L.map('mapdiv',{
     center: [46.5455, -66.7362],
     zoom: 8
 });
+
 var osm=new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-var rights = L.esri.featureLayer({
-    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/2"
+var layers = L.esri.dynamicMapLayer({
+    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/",
+    layers: [0],
+    useCors: false
 })
 .addTo(map);
 
-var utilities = L.esri
-    .featureLayer({
-        url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/1"
-    })
-    .addTo(map);
+var seafood = L.esri.dynamicMapLayer({
+    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/",
+    layers: [0],
+    useCors: false
+})
+.addTo(map);
 
-var seafood = L.esri
-    .featureLayer({
-    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/0",
-    pointToLayer: function (geojson, latlng) {
-        // return L.circleMarker(latlng, geojsonMarkerOptions);
-        return L.circleMarker(latlng);
-    }
-    })
-    .addTo(map);
+var utility = L.esri.dynamicMapLayer({
+    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/",
+    layers: [1],
+    useCors: false
+})
+.addTo(map);
+
+var ongrights = L.esri.dynamicMapLayer({
+    url: "https://humpback1:6443/arcgis/rest/services/AlternativeGCX/Test/MapServer/",
+    layers: [2],
+    useCors: false
+})
+.addTo(map);
+
+console.log(layers);
 
 // var query = rights.setWhere("TENURE_NUMBER_ID = 'M00021679'")
 
@@ -40,9 +50,10 @@ queryString = queryString.substring(1);
 // map.fitBounds([[coords[0],coords[1]],[coords[2],coords[3]]]); //London
 
 if (queryString){
-    rights.query().where(`TENURE_NUMBER_ID = '${queryString}'`).bounds(function (error, latLngBounds, response) {
+    ongrights.query().layer(2).where(`TENURE_NUMBER_ID = '${queryString}'`).bounds(function (error, latLngBounds, response) {
         if (error) {
         console.log(error);
+        console.log(latLngBounds)
         return;
         }
         map.fitBounds(latLngBounds);
