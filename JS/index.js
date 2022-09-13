7dc1b09ffd6332e7f8ab16e0726673742a6ca24f
DannaC17 <dannajuliette02@gmail.com>
2022-09-13 11:05:27 -0700

    Adding features: Icon, Your Location, Zoom Home, Print, Splash page, Context menu, CRS Dropdown, Placeholder

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,163 @@
 import { selectionEventHandler } from './selectionHandler.js';
 
 // Map
-var latlngMap = [46.5455, -66.7362];
+const zoomMap = 8;
+const latMap = 46.5455;
+const lngMap = -66.7362;
 
 var mapOptions = {
     fullscreenControl: true,
     measureControl: false,
     zoomsliderControl: true,
     zoomControl: false,
-    pmIgnore: false
+    pmIgnore: false,
+    minZoom: 3,
+    maxZoom: 15,
+    contextmenu: true,
+    contextmenuWidth: 140,
+	contextmenuItems: [{
+	    text: 'Show coordinates',
+	    callback: showCoordinates
+	}, {
+	    text: 'Center map here',
+	    callback: centerMap
+	}, '-', {
+	    text: 'Zoom in',
+	    // icon: 'images/zoom-in.png',
+	    callback: zoomIn
+	}, {
+	    text: 'Zoom out',
+	    // icon: 'images/zoom-out.png',
+	    callback: zoomOut
+	}]
 }
 
-var map = L.map('mapdiv',mapOptions).setView(latlngMap, 8);
+var map = L.map('mapdiv',mapOptions).setView([latMap, lngMap], zoomMap);
 var defaultBase = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+//Locate control
+map.addControl(L.control.locate({
+    locateOptions: {
+            enableHighAccuracy: true
+    },
+    strings: {
+        title: "Your Location"
+    }
+}));
+
+//CRS Control
+var crsinfo = L.Control.extend({
+
+    options: {position: "bottomleft"},
+    onAdd: function(map) {    
+        var className = 'pgts-mapselection';    
+        var container = L.DomUtil.create('div', 'crsinfo');
+        var select = leaflet.DomUtil.create('select', className, container);
+        var option1 = leaflet.DomUtil.create('option', className, select);
+        var option2 = leaflet.DomUtil.create('option', className, select);
+
+        option1.innerHTML = "EPSG1";
+        option2.innerHTML = "EPSG2";
+        L.DomEvent.on( select, 'change', this.onSet, this);
+        return container;
+    },
+    
+    onSet: function() {
+        console.log(map.options.crs.code);
+    }
+});
+
+map.addControl(new crsinfo());
+
+// const currentCRS = L.Control.extend({
+//     options: {
+//       position: "bottomleft",
+//     },
+  
+//     onAdd: function (map) {
+
+//       const currentCRS = L.DomUtil.create("p");
+//       currentCRS.title = "EPSG";
+//       currentCRS.innerHTML = map.options.crs.code;
+  
+//       return currentCRS;
+//     },
+// });
+
+// map.addControl(new currentCRS());
+
+//Zoom Home 
+const htmlTemplate =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 18.451L16 6.031 0 18.451v-5.064L16 .967l16 12.42zM28 18v12h-8v-8h-8v8H4V18l12-9z" /></svg>';
+
+// create custom button
+const zoomHome = L.Control.extend({
+    // button position
+    options: {
+      position: "topleft",
+    },
+  
+    // method
+    onAdd: function (map) {
+      // create button
+      const btn = L.DomUtil.create("button");
+      btn.title = "Initial View";
+      btn.innerHTML = htmlTemplate;
+      btn.className += "leaflet-bar back-to-home hidden";
+  
+      return btn;
+    },
+});
+
+// adding new button to map control
+map.addControl(new zoomHome());
+
+// on drag end
+map.on("moveend", getCenterOfMap);
+
+const buttonBackToHome = document.querySelector(".back-to-home");
+
+function getCenterOfMap() {
+    buttonBackToHome.classList.remove("hidden");
+
+    buttonBackToHome.addEventListener("click", () => {
+        map.flyTo([latMap, lngMap], zoomMap);
+    });
+
+    map.on("moveend", () => {
+        const { lat: latCenter, lng: lngCenter } = map.getCenter();
+
+        const latC = latCenter.toFixed(3) * 1;
+        const lngC = lngCenter.toFixed(3) * 1;
+
+        const defaultCoordinate = [+latMap.toFixed(3), +lngMap.toFixed(3)];
+
+        const centerCoordinate = [latC, lngC];
+
+        if (compareToArrays(centerCoordinate, defaultCoordinate)) {
+        buttonBackToHome.classList.add("hidden");
+        }
+    });
+}
+
+const compareToArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
+//Context menu
+function showCoordinates (e) {
+	console.log(e.latlng);
+}
+
+function centerMap (e) {
+	map.panTo(e.latlng);
+}
+
+function zoomIn (e) {
+	map.zoomIn();
+}
+
+function zoomOut (e) {
+	map.zoomOut();
+}
 
 // Layers
 var ongrights = L.esri.featureLayer({
@@ -136,15 +281,17 @@ L.PM.setOptIn(false);
 //Allow toolbar options just after event
 map.on('pm:create', (e) => {
     ongrights.setStyle({ pmIgnore: true });
-    grid.setStyle({ pmIgnore: true });
+    // grid.setStyle({ pmIgnore: true });
     L.PM.reInitLayer(e.layer);    
   });
-  
-var browserControl = L.control.browserPrint({printModes: ["Portrait", "Landscape", "Auto"], documentTitle:'PGTS-Viewer'}).addTo(map);
+
+//Print  
+var browserControl = L.control.browserPrint({printModes: ["Portrait", "Landscape", "Auto"], documentTitle:'PGTS-Viewer', title:"Print Map"}).addTo(map);
 
 // Legend
 L.esri.legendControl(seafood, { position: 'topright' }).addTo(map);
 
+//Search location
 const provider = new GeoSearch.OpenStreetMapProvider();
 
 const searchControl = new GeoSearch.GeoSearchControl({
@@ -154,6 +301,50 @@ const searchControl = new GeoSearch.GeoSearchControl({
 });
 
 map.addControl(searchControl);
+
+//Control Placeholders
+(function addControlPlaceholders(map) {
+    let corners = map._controlCorners;
+    let l = "leaflet-";
+    let container = map._controlContainer;
+  
+    function createCorner(vSide, hSide) {
+      let className = l + vSide + " " + l + hSide;
+      corners[vSide + hSide] = L.DomUtil.create("div", className, container);
+    }
+  
+    createCorner("top", "center");
+    createCorner("bottom", "center");
+    createCorner("vertical", "left");
+    createCorner("vertical", "right");
+  })(map);
+  
+  // --------------------------------------------------------------
+  const configs = [
+    // { position: "topcenter", description: "top description" },
+    { position: "bottomcenter", description: map.options.crs.code },
+    // { position: "verticalleft", description: "left description" },
+    // { position: "verticalright", description: "right description" },
+  ];
+  
+  configs.forEach((item) => {
+    L.Control.Search = L.Control.extend({
+      options: {
+        position: item.position,
+      },
+      onAdd: function () {
+        const container = L.DomUtil.create("div", "description");
+  
+        L.DomEvent.disableClickPropagation(container);
+  
+        container.insertAdjacentHTML("beforeend", item.description);
+  
+        return container;
+      },
+    });
+  
+    new L.Control.Search().addTo(map);
+  });
 
 // Query (Find by Tenure ID)
 var queryString = window.location.search;
@@ -193,7 +384,6 @@ if (queryString && queryString !== 'MapSelection'){
 // M00021658
 // M00015995
 }
-
 
 // Map Selection
 if (queryString && queryString === 'MapSelection'){
